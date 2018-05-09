0e8699a2152136a946e0fa637dd15d5a2b598f28
varakasananas <38427712+varakasananas@users.noreply.github.com>
2018-05-09 10:20:06 +0100

    preloader js

--- js/theme.js
+++ js/theme.js
@@ -1,90 +1,110 @@
 /*!
  * */
-!function(t){"use strict";t("a.page-scroll").bind("click",function(a){var o=t(this);t("html, body").stop().animate({scrollTop:t(o.attr("href")).offset().top-50},1250,"easeInOutExpo"),a.preventDefault()}),t("body").scrollspy({target:".navbar-fixed-top",offset:100}),t(".navbar-collapse ul li a").click(function(){t(".navbar-toggle:visible").click()}),t("#mainNav").affix({offset:{top:50}})}(jQuery);
+!function (t) {
+    "use strict";
+    t("a.page-scroll").bind("click", function (a) {
+        var o = t(this);
+        t("html, body").stop().animate({scrollTop: t(o.attr("href")).offset().top - 50}, 1250, "easeInOutExpo"), a.preventDefault()
+    }), t("body").scrollspy({
+        target: ".navbar-fixed-top",
+        offset: 100
+    }), t(".navbar-collapse ul li a").click(function () {
+        t(".navbar-toggle:visible").click()
+    }), t("#mainNav").affix({offset: {top: 50}})
+}(jQuery);
 
+window.onload = function () {
+    /* preloader*/
+    setTimeout(function () {
+        /*document.getElementById('page-preloader').classList.add('loaded');*/
+        $("#loader").fadeOut();
+
+    }, 1500)
+};
 
 function setLowVolume() {
-    var myAudio = document.getElementById("audio1");
+    let myAudio = document.getElementById("audio1");
     myAudio.volume = 0.3; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
 }
 
-$(document).ready(function(){
+
+$(document).ready(function () {
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-        loop:true,
-        margin:5,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
+        loop: true,
+        margin: 5,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
         autoHeight: true,
-        responsive:{
-            0:{
-                items:2
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
 
     });
-/*    owl.on('mousewheel', '.owl-stage', function (e) {
-        if (e.deltaY>0) {
-            owl.trigger('next.owl');
-        } else {
-            owl.trigger('prev.owl');
-        }
-        e.preventDefault();
-    });*/
+    /*    owl.on('mousewheel', '.owl-stage', function (e) {
+            if (e.deltaY>0) {
+                owl.trigger('next.owl');
+            } else {
+                owl.trigger('prev.owl');
+            }
+            e.preventDefault();
+        });*/
     $('#animated-thumbnials').lightGallery({
         selector: '.item',
-        thumbnail:true,
+        thumbnail: true,
         animateThumb: false,
         showThumbByDefault: false,
-        download:false
+        download: false
     });
-/*    lightGallery(document.getElementById('animated-thumbnials'), {selector: '.item'});*/
+    /*    lightGallery(document.getElementById('animated-thumbnials'), {selector: '.item'});*/
 
-/*2 owl*/
+    /*2 owl*/
     var owl2 = $('.owl-animation');
     owl2.owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        autoplayTimeout:5000,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 5000,
         slideSpeed: 3000,
         smartSpeed: 4000,
-        autoplayHoverPause:true,
+        autoplayHoverPause: true,
         autoHeight: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:2
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
 
     });
-/*    owl2.on('mousewheel', '.owl-stage', function (e) {
-        if (e.deltaY>0) {
-            owl.trigger('next.owl');
-        } else {
-            owl.trigger('prev.owl');
-        }
-        e.preventDefault();
-    });*/
+    /*    owl2.on('mousewheel', '.owl-stage', function (e) {
+            if (e.deltaY>0) {
+                owl.trigger('next.owl');
+            } else {
+                owl.trigger('prev.owl');
+            }
+            e.preventDefault();
+        });*/
     $('#animated-thumbnials2').lightGallery({
         selector: '.item',
-        thumbnail:true,
+        thumbnail: true,
         animateThumb: false,
         showThumbByDefault: false,
-        download:false
+        download: false
     });
 });
